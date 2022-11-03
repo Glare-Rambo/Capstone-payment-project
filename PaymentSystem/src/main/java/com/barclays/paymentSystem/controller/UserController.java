1d68633f2ab5fd61ec23f7404c2e70e0c02711d6
DESKTOP-3SSIEOM\Dell <singhkhushi8808@gmail.com>
2022-11-03 16:22:57 +0530

    Created User entity  || created user service for get-all-user || created user controller for get-all-user

--- PaymentSystem/src/main/java/com/barclays/paymentSystem/controller/UserController.java
+++ PaymentSystem/src/main/java/com/barclays/paymentSystem/controller/UserController.java
@@ -1,11 +1,18 @@
 package com.barclays.paymentSystem.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.barclays.paymentSystem.entity.User;
+import com.barclays.paymentSystem.exception.PaymentsException;
 import com.barclays.paymentSystem.service.UserService;
 
 @RestController
@@ -18,4 +25,10 @@ public class UserController {
 
 	@Autowired
 	private Environment environment;
+	
+	@GetMapping(value = "/get-all-user")
+	public ResponseEntity<List<User>> getAllusers() throws PaymentsException {
+		List<User> userList = userService.getAllUsers();
+		return new ResponseEntity<>(userList, HttpStatus.OK);
+	}
 }
--- PaymentSystem/src/main/java/com/barclays/paymentSystem/service/UserService.java
+++ PaymentSystem/src/main/java/com/barclays/paymentSystem/service/UserService.java
@@ -1,17 +1,26 @@
 package com.barclays.paymentSystem.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
+import com.barclays.paymentSystem.entity.User;
 import com.barclays.paymentSystem.repository.UserRepository;
+
+
 
 @Service(value = "userService")
 public class UserService {
 
 	@Autowired
-	UserRepository userRespository;
+	UserRepository userRepository;
 
 	@Autowired
 	private Environment environment;
+	
+	public List<User> getAllUsers() {
+		return userRepository.findAll();
+	}
 }
