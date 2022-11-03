ff805597f68ef8cb9fd1ba755bf7933607e2cde6
Glare-Rambo <himanshut.work@gmail.com>
2022-11-03 21:59:17 +0530

    Bill generating code

--- PaymentSystem/src/main/java/com/barclays/paymentSystem/controller/BillController.java
+++ PaymentSystem/src/main/java/com/barclays/paymentSystem/controller/BillController.java
@@ -1,10 +1,21 @@
 package com.barclays.paymentSystem.controller;
 
+import java.util.List;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.barclays.paymentSystem.entity.Bill;
+import com.barclays.paymentSystem.exception.PaymentsException;
 import com.barclays.paymentSystem.service.BillService;
 
 @RestController
@@ -14,4 +25,19 @@ public class BillController {
 
 	@Autowired
 	BillService billsService;
+	
+
+	@PostMapping(value = "/generateBill")
+	public ResponseEntity<String> generatebill(@Valid @RequestBody Bill bills)
+			throws PaymentsException {
+		
+		return billsService.generateBill(bills);
+		
+	}
+	
+	@GetMapping(value ="/getBills/{billerCode}")
+	public ResponseEntity<List<String>> getbills(@PathVariable Integer billerCode) throws PaymentsException {
+		return billsService.getBills(billerCode);
+		
+	}
 }
--- PaymentSystem/src/main/java/com/barclays/paymentSystem/repository/BillRepository.java
+++ PaymentSystem/src/main/java/com/barclays/paymentSystem/repository/BillRepository.java
@@ -1,9 +1,12 @@
 package com.barclays.paymentSystem.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.barclays.paymentSystem.entity.Bill;
 
 public interface BillRepository extends JpaRepository<Bill, Integer>{
 
+	List<Bill> findByBillerCode(Integer billerCode);
 }
