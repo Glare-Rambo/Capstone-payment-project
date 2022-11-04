25afc45e1625319f02d52bacd8941277d7ecaaa3
Glare-Rambo <himanshut.work@gmail.com>
2022-11-04 09:35:31 +0530

    Updating project

--- PaymentSystem/src/main/java/com/barclays/paymentSystem/exception/PaymentsException.java
+++ PaymentSystem/src/main/java/com/barclays/paymentSystem/exception/PaymentsException.java
@@ -1,8 +1,17 @@
 package com.barclays.paymentSystem.exception;
 
-public class PaymentsException extends Exception {
+public class PaymentsException extends RuntimeException {
 
 	private static final long serialVersionUID = 1L;
+	
+	
+	
+
+	public PaymentsException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
 
 	public PaymentsException(String message) {
 		super(message);
