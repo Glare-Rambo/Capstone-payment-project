45eb1b7e7b7d81077a31be55bbd5a28fb2bc803b
kanha <saikrishna.at2021@gmail.com>
2022-11-04 09:43:18 +0530

    test methods added

--- PaymentSystem/src/test/java/com/barclays/PaymentSystem/PaymentSystemApplicationTests.java
+++ PaymentSystem/src/test/java/com/barclays/PaymentSystem/PaymentSystemApplicationTests.java
@@ -1,13 +1,48 @@
 package com.barclays.PaymentSystem;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import com.barclays.paymentSystem.entity.User;
+import com.barclays.paymentSystem.exception.PaymentsException;
+import com.barclays.paymentSystem.repository.UserRepository;
+import com.barclays.paymentSystem.service.UserService;
 
 @SpringBootTest
 class PaymentSystemApplicationTests {
 
+
+	@Mock
+	UserRepository userRepository;
+	
+	@InjectMocks
+	UserService userService = new UserService();
+	
 	@Test
-	void contextLoads() {
+	public void authenticateUserTestValidCredentials() throws PaymentsException {
+		
+		User User = new User();
+		User.setLoginId(1000001);
+		User.setPassword("123");
+		ResponseEntity<String> resp=new ResponseEntity<>("User logged in Successfully with User id: User [ loginId= "+User.getLoginId().shortValue()+"password=" +User.getPassword()+"]", HttpStatus.OK);
+		Mockito.when(userService.loginUser(User)).thenReturn(resp);
+		//("User added Successfully with User id: User [ loginId= "+User.getLoginId().shortValue()+"password=" +User.getPassword()+"]")
+		ResponseEntity<String> actual = userService.loginUser(User);
+		Assertions.assertEquals("User logged in Successfully with User id: User [ loginId= "+User.getLoginId().shortValue()+"password=" +User.getPassword()+"]", actual);
+	} 
+	@Test
+	public void authenticateUserTestInValidCredentials() throws PaymentsException {
+		User User = new User();
+		User.setLoginId(1000001);
+		User.setPassword("1");
+	    PaymentsException exception=Assertions.assertThrows(PaymentsException.class,()->userService.loginUser(User));
+		Assertions.assertEquals("Service.INCORRECT", exception.getMessage());
 	}
 
 }
